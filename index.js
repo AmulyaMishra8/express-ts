615315e25b902d821585a25491bb166c2477e90c
AmulyaMishra8 <amulyamishra1733@gmail.com>
2025-05-05 22:45:53 +0530

    zod and middle wares

--- index.js
+++ index.js
@@ -1,77 +1,122 @@
 "use strict";
+// import express, { Request, Response } from 'express';
+// const app = express();
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+// enum statusCode{
+//     Success = 200,
+//     ResourceCreated = 201,
+//     badRequest = 400,
+//     unauthorized = 401
+// }
+// const port : number = 3000;
+// interface kidneys {
+//     healthy : boolean;
+// }
+// interface UserSchema {
+//     id : number,
+//     name : string;
+//     kidney : kidneys[]
+// }
+// const user : UserSchema[] = [{
+//     id : 1,
+//     name : 'John',
+//     kidney : [{
+//         healthy : false
+//     },
+//     {
+//         healthy : true
+//     }]
+// },
+// {
+//     id : 2,
+//     name : 'Jane',
+//     kidney : [{
+//         healthy : true
+//     },
+//     {
+//         healthy : true
+//     }]
+// },
+// {
+//     id : 3,
+//     name : 'Jain',
+//     kidney : [{
+//         healthy : false
+//     },
+//     {
+//         healthy : false
+//     }]
+// }]
+// let id : number;
+// app.get('/', (req : Request, res : Response) => {
+//     let strId = req.query.id;
+//     let userKidney !: kidneys[];
+//     let foundKidney;
+//     if(typeof strId !== 'string'){     
+//         res.status(statusCode.badRequest).json({error: 'ID must be a string'})
+//         return
+//     }
+//     id = parseInt(strId);
+//     if (isNaN(id)) {
+//         res.status(statusCode.badRequest).json({ error: 'ID must be a valid number' });
+//         return
+//     }
+//     for(let i = 0; i < user.length; i++){
+//         if(user[i].id == id){
+//             userKidney = user[i].kidney;
+//             foundKidney = true;
+//             break;
+//         }
+//     }
+//     if(!foundKidney){
+//          res.status(statusCode.unauthorized).json({error: 'ID is not valid'})
+//          return
+//         }
+//     res.status(statusCode.Success).json({ 
+//         userKidney : userKidney
+//     });
+// })
+// app.post('/', )
+// app.listen(port, ()=>{
+//     console.log(`app is running on port ${port}`)
+// })
 const express_1 = __importDefault(require("express"));
+const zod_1 = require("zod");
 const app = (0, express_1.default)();
 var statusCode;
 (function (statusCode) {
-    statusCode[statusCode["Success"] = 200] = "Success";
-    statusCode[statusCode["ResourceCreated"] = 201] = "ResourceCreated";
+    statusCode[statusCode["success"] = 200] = "success";
     statusCode[statusCode["badRequest"] = 400] = "badRequest";
-    statusCode[statusCode["unauthorized"] = 401] = "unauthorized";
 })(statusCode || (statusCode = {}));
-const port = 3000;
-var user = [{
-        id: 1,
-        name: 'John',
-        kidney: [{
-                healthy: false
-            },
-            {
-                healthy: true
-            }]
-    },
-    {
-        id: 2,
-        name: 'Jane',
-        kidney: [{
-                healthy: true
-            },
-            {
-                healthy: true
-            }]
-    },
-    {
-        id: 3,
-        name: 'Jain',
-        kidney: [{
-                healthy: false
-            },
-            {
-                healthy: false
-            }]
-    }];
-let id;
-app.get('/', (req, res) => {
-    let strId = req.query.id;
-    let userKidney;
-    let foundKidney;
-    if (typeof strId !== 'string') {
-        res.status(statusCode.badRequest).json({ error: 'ID must be a string' });
+const usernameSchema = zod_1.z.string().email();
+const passwordSchema = zod_1.z.string();
+function userMiddleware(req, res, next) {
+    const email = req.header('username');
+    const username = usernameSchema.safeParse(email);
+    const pass = req.header('password');
+    const password = passwordSchema.safeParse(pass);
+    if (!username.success || !password.success) {
+        res.status(statusCode.badRequest).json({
+            message: "input type wrong"
+        });
         return;
     }
-    id = parseInt(strId);
-    if (isNaN(id)) {
-        res.status(statusCode.badRequest).json({ error: 'ID must be a valid number' });
+    if (email != "amulyamishra1733@gmail.com" || pass != "1234") {
+        res.status(statusCode.badRequest).json({
+            message: "Incorrect Input"
+        });
         return;
     }
-    for (let i = 0; i < user.length; i++) {
-        if (user[i].id == id) {
-            userKidney = user[i].kidney;
-            foundKidney = true;
-            break;
-        }
+    else {
+        next();
     }
-    if (!foundKidney) {
-        res.status(statusCode.unauthorized).json({ error: 'ID is not valid' });
-        return;
-    }
-    res.status(statusCode.Success).json({
-        userKidney: userKidney
-    });
+}
+app.get('/', userMiddleware, (req, res) => {
+    res.status(statusCode.success).send("you are authenticated");
 });
-app.listen(port, () => {
-    console.log(`app is running on port ${port}`);
+app.listen(3000, () => {
+    console.log(`the site is running at local host 3000`);
 });
--- index.ts
+++ index.ts
@@ -1,94 +1,143 @@
-import express, { Request, Response } from 'express';
+// import express, { Request, Response } from 'express';
+// const app = express();
+
+
+// enum statusCode{
+//     Success = 200,
+//     ResourceCreated = 201,
+//     badRequest = 400,
+//     unauthorized = 401
+// }
+
+// const port : number = 3000;
+
+// interface kidneys {
+//     healthy : boolean;
+// }
+// interface UserSchema {
+//     id : number,
+//     name : string;
+//     kidney : kidneys[]
+// }
+
+// const user : UserSchema[] = [{
+//     id : 1,
+//     name : 'John',
+//     kidney : [{
+//         healthy : false
+//     },
+//     {
+//         healthy : true
+//     }]
+// },
+// {
+//     id : 2,
+//     name : 'Jane',
+//     kidney : [{
+//         healthy : true
+//     },
+//     {
+//         healthy : true
+//     }]
+// },
+// {
+//     id : 3,
+//     name : 'Jain',
+//     kidney : [{
+//         healthy : false
+//     },
+//     {
+//         healthy : false
+//     }]
+// }]
+
+// let id : number;
+// app.get('/', (req : Request, res : Response) => {
+//     let strId = req.query.id;
+//     let userKidney !: kidneys[];
+//     let foundKidney;
+//     if(typeof strId !== 'string'){     
+//         res.status(statusCode.badRequest).json({error: 'ID must be a string'})
+//         return
+//     }
+//     id = parseInt(strId);
+    
+//     if (isNaN(id)) {
+//         res.status(statusCode.badRequest).json({ error: 'ID must be a valid number' });
+//         return
+//     }
+    
+
+//     for(let i = 0; i < user.length; i++){
+//         if(user[i].id == id){
+//             userKidney = user[i].kidney;
+//             foundKidney = true;
+//             break;
+//         }
+//     }
+
+//     if(!foundKidney){
+//          res.status(statusCode.unauthorized).json({error: 'ID is not valid'})
+//          return
+//         }
+//     res.status(statusCode.Success).json({ 
+//         userKidney : userKidney
+//     });
+// })
+
+
+// app.post('/', )
+
+
+// app.listen(port, ()=>{
+//     console.log(`app is running on port ${port}`)
+// })
+
+
+
+import express,{Request, Response, NextFunction} from 'express'
+import {z} from 'zod';
 const app = express();
 
 
 enum statusCode{
-    Success = 200,
-    ResourceCreated = 201,
-    badRequest = 400,
-    unauthorized = 401
+    success = 200,
+    badRequest = 400
 }
 
-const port : number = 3000;
+const usernameSchema = z.string().email();
+const passwordSchema = z.string();
 
-interface kidneys {
-    healthy : boolean;
-}
-interface UserSchema {
-    id : number,
-    name : string;
-    kidney : kidneys[]
-}
 
-const user : UserSchema[] = [{
-    id : 1,
-    name : 'John',
-    kidney : [{
-        healthy : false
-    },
-    {
-        healthy : true
-    }]
-},
-{
-    id : 2,
-    name : 'Jane',
-    kidney : [{
-        healthy : true
-    },
-    {
-        healthy : true
-    }]
-},
-{
-    id : 3,
-    name : 'Jain',
-    kidney : [{
-        healthy : false
-    },
-    {
-        healthy : false
-    }]
-}]
-
-let id : number;
-app.get('/', (req : Request, res : Response) => {
-    let strId = req.query.id;
-    let userKidney !: kidneys[];
-    let foundKidney;
-    if(typeof strId !== 'string'){     
-        res.status(statusCode.badRequest).json({error: 'ID must be a string'})
+function userMiddleware(req : Request, res : Response, next : NextFunction ){
+    const email = req.header('username');
+    const username = usernameSchema.safeParse(email);
+    const pass = req.header('password');
+    const password = passwordSchema.safeParse(pass);
+    if(!username.success || !password.success){
+        res.status(statusCode.badRequest).json({
+            message : "input type wrong"
+        })
         return
     }
-    id = parseInt(strId);
-    
-    if (isNaN(id)) {
-        res.status(statusCode.badRequest).json({ error: 'ID must be a valid number' });
+
+    if(email != "amulyamishra1733@gmail.com" || pass != "1234"){
+        res.status(statusCode.badRequest).json({
+            message : "Incorrect Input"
+        })
         return
     }
-    
-
-    for(let i = 0; i < user.length; i++){
-        if(user[i].id == id){
-            userKidney = user[i].kidney;
-            foundKidney = true;
-            break;
-        }
+    else{
+        next();
     }
+}
 
-    if(!foundKidney){
-         res.status(statusCode.unauthorized).json({error: 'ID is not valid'})
-         return
-        }
-    res.status(statusCode.Success).json({ 
-        userKidney : userKidney
-    });
+
+app.get('/', userMiddleware, (req : Request, res : Response) =>{
+    res.status(statusCode.success).send("you are authenticated");
 })
 
 
-app.post('/', )
-
-
-app.listen(port, ()=>{
-    console.log(`app is running on port ${port}`)
+app.listen(3000, ()=>{
+    console.log(`the site is running at local host 3000`)
 })
